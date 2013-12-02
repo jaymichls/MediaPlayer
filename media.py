f6df616a40b7650b86e3c7218cd7e271f499f4d3
Jason <jaymichls@gmail.com>
2013-12-01 23:40:25 -0800

    Fixed up defaults in constuctor

--- media.py
+++ media.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-class Movie:
-	def __init__(self):
-		self.title = ''
-		self.rating = 0
-		self.releaseDate = '' 
-		
-	def __init__(self, title, rating, releaseDate):
+class Movie:		
+	def __init__(self, title='', rating=0, releaseDate=''):
 		self.title = title
 		self.rating = rating
 		self.releaseDate = releaseDate 
@@ -27,15 +22,7 @@ class Movie:
 		return retVal
 
 class TVShow:
-	def __init__(self):
-		self.showName = ''
-		self.rating = 0
-		self.releaseDate = ''
-		self.plot = ''
-		self.genre = '' 
-		self.numberOfSeasons = 0
-		
-	def __init__(self, showName, numberOfSeasons):
+	def __init__(self, showName='', numberOfSeasons=''):
 		self.showName = showName
 		self.rating = 0
 		self.releaseDate = '' 
@@ -43,38 +30,19 @@ class TVShow:
 		self.genre = ''
 		self.numberOfSeasons = numberOfSeasons
 		
-	def setShowname(self, showName):
-		self.showName = showName
-
-	def setRating(self, rating):
-		self.rating = rating
-
-	def setReleaseDate(self, releaseDate):
-		self.releaseDate = releaseDate
+	def __dir__(self):
+		return ['showName','rating','releaseDate','plot','genre','numberOfSeasons']
 
 class TVSeason:
-	def __init__(self):
-		self.showName = ''
-		self.seasonNumber = 0
-		self.seasonLocation = ''
-		
-	def __init__(self, showName, seasonNumber):
+	def __init__(self, showName='', seasonNumber=''):
 		self.showName = showName
 		self.seasonNumber = seasonNumber
-		self.sesasonLocation = ''
+		
+	def __dir__(self):
+		return ['showName','seasonNumber']
 	
 class TVEpisode:
-	def __init__(self):
-		self.episodeName = ''
-		self.episodeNumber = ''
-		self.showName = ''
-		self.seasonNumber = 0
-		self.filePath = ''
-		self.fileName = ''
-		self.quality = ''
-		self.fileExtension = ''
-		
-	def __init__(self, episodeName, episodeNumber,showName,seasonNumber, filePath, fileName, quality, fileExtension):
+	def __init__(self, episodeName='', episodeNumber=0,showName='',seasonNumber=0, filePath='', fileName='', quality='', fileExtension=''):
 		self.episodeName = episodeName
 		self.episodeNumber = episodeNumber
 		self.showName = showName
@@ -83,19 +51,6 @@ class TVEpisode:
 		self.fileName = fileName
 		self.quality = quality
 		self.fileExtension = fileExtension
-		
-	def setEpisodeName(self, episodeName):
-		self.episodeName = episodeName
-
-	def setEpisodeNumber(self, episodeNumber):
-		self.episodeNumber = episodeNumber		
 	
-	def setFilePath(self, filePath):
-		self.filePath = filePath
-		
-	def setFileName(self, fileName):
-		self.fileName = fileName
-		
-	def setQuality(self, quality):
-		self.quality = quality
-		
+	def __dir__(self):
+		return ['episodeName','episodeNumber','showName','seasonNumber','filePath','fileName','quality','fileExtension']
