4980889693274e7ba71e5ab1c51d7b98017d8beb
Jason <jaymichls@gmail.com>
2013-12-03 00:26:58 -0800

    Created modified read files method and dbSize/Speed test

--- mediaLibrary.py
+++ mediaLibrary.py
@@ -5,7 +5,7 @@ import os, sys
 import media 
 import dbConnection as dbcon
 import re
-import inspect
+import timeit
 
 def importMedia():
 	#A foler name as a parameter and if it's TV or Movies
@@ -13,14 +13,14 @@ def importMedia():
 		return
 	directoryPath = sys.argv[1]
 	mediaOption = sys.argv[2]
-	print directoryPath
-	print mediaOption
-	mediaDB = dbcon.Database('Media')
+
+	mediaDB = dbcon.Database('Media1')
 	
 	#Get a list of all of the media 
 	if mediaOption == '-t':
 		print 'Tv Shows'
-		tvShows, tvSeasons, tvEpisodes = allTVShows(directoryPath)
+		tvShows, tvSeasons, tvEpisodes = allTVShows1(directoryPath)
+		
 		importTvShows(mediaDB, tvShows, tvSeasons, tvEpisodes)
 
 
@@ -41,7 +41,7 @@ def importTvShows(tvDB, tvShows, tvSeasons, tvEpisodes):
 def insertList(db, tableName, someList):
 	#Insert data into tables. 
 	for item in someList:
-		print 'Inserting: ', item.__dict__
+		#print 'Inserting: ', item.__dict__
 		db.insertInto(tableName, item.__dict__)
 		
 #Movie Specific
@@ -63,17 +63,19 @@ def allTVShows(directoryPath):
 		
 		#gets the name of the show from the file path also the season # if there is one
 		showSeason = root.split('/')
-		showName = showSeason[6]
-		
+		showName = showSeason[showPosition]
+		print showSeason
 		#Get all of the seasons that are available for the current show
 		if files == [] and showName in tempShowList: 
 			seasonCount = len(dirs)
+			#look for folder.jpg ignore anything else Thumbs.db or *.ini 
 			showList.append(media.TVShow(showName,seasonCount))
+			
 
 		# Only deal with episodes and use the path from the root
 		elif files != []: 
 			season = (showSeason[7].split(' '))[1] # Gets the season number
-			seasonList.append(media.TVSeason(showName, season))
+			seasonList.append(media.TVSeason(showName, season)) # maybe this should be done after or a search to see if there is a folder.jpg			
 			for f in files:
 				seasonNumber,episodeNumber, quality, fileName, fileExtension = stripFileInformation(f)
 				if int(seasonNumber) == int(season): #confirm that the episode is from the current season
@@ -82,7 +84,60 @@ def allTVShows(directoryPath):
 	print 'Done Reading files'
 	return showList, seasonList, episodeList
 
+def allTVShows1(directoryPath):
+	print 'Starting to read files'
+	#initialize vars
+	showPosition = len(directoryPath.split('/'))
+	seasonPosition = showPosition + 1
+	
+	showList = []
+	seasonList = []
+	episodeList = []
+	currentShow = ''
+	
+	for root, dirs, files in os.walk(directoryPath):
+		if root == directoryPath:
+			tempShowList = dirs
+			continue
+		
+		newShow = root.split('/')
+		showName = newShow[showPosition]
+		
+		if showName == currentShow:	#Get show season episides and folder.jpg
+			#See if there is a folder.jpg 
+			seasonFolderImage = [f for f in files if f == 'folder.jpg']
+			#Number of episodes is the number of files unless there is a folder.jpg
+			if seasonFolderImage == '': numberOfEpisodes = len(files)
+			else: numberOfEpisodes = len(files) - 1
+			#if this is in fact a season of a show the season number will be the seasonPosition in the newShow (7)
+			season = (newShow[seasonPosition].split(' '))[1] # Gets the season number
+			#Create a new Season with above
+			seasonList.append(media.TVSeason(showName, season,seasonFolderImage, numberOfEpisodes))		
+			#Go through each file of the current season except folder.jpg
+			for f in files:				
+				#stripFileInformation from the file 
+				if f != 'folder.jpg' and f != 'Thumbs.db': 					
+					seasonNumber,episodeNumber, quality, fileName, fileExtension = stripFileInformation(f)
+				#Create new Epsiode 
+				if int(seasonNumber) == int(season): #confirm that the episode is from the current season
+					episodeList.append(media.TVEpisode('',episodeNumber,showName, seasonNumber,'',fileName,quality,fileExtension))
+					
+		elif showName != currentShow:	#New show get folder.jpg
+			#Number of season is the length of the dirs 
+			numberOfSeason = len(dirs)
+			#folder.jpg should be the only file if not ditch the rest.
+			showFolderImage = [f for f in files if f == 'folder.jpg']				
+			#Create new show with newShow name
+			showList.append(media.TVShow(showName,numberOfSeason,showFolderImage))
+			#Set currentShow to showName 
+			currentShow = showName
+			
+	print 'Done Reading files'
+	return showList, seasonList, episodeList
+
+
 #Regular expressions and os path used to get meaningful information from the filename	
+#TODO account for stupid season and episode number at the start of the file. Eventually... See Entourage
 def stripFileInformation(f):
 	regexEpisode = re.compile("(?:e|episode|\n)(\d{2})",re.IGNORECASE)
 	regexSeason = re.compile("(?:s|season)(\d{2})",re.IGNORECASE)
@@ -94,7 +149,19 @@ def stripFileInformation(f):
 	q = regexQuality.findall(f)[0]
 
 	return s, e, q, fileName, fileExtension
-	
-importMedia()
+
+def dbSizeTest():
+	testDB = dbcon.Database ('dbSizeTest')
+	testDB.createTable('Episode', dir(media.TVEpisode()))
+	test = media.TVEpisode('Test',0,'Some Name',12,'testes','tetset','testes','testes')
+	for i in xrange(1000):
+		test.episodeNumber = i
+		testDB.insertInto('Episode', test.__dict__)
+
+if __name__ == '__main__':
+	importMedia()
+
+#	print(timeit.timeit("dbSizeTest()", setup="from __main__ import dbSizeTest", number=10))
+
 #tvshowdir = '/home/jay/Documents/MediaPlayer/Tv Shows'
 #allTVShows(tvshowdir)
